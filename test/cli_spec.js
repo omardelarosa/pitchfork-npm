605762df8480952c7c82fb23803c0537ac4928cb
omardelarosa <thedelarosa@gmail.com>
2014-07-16 16:58:21 -0400

    added cli tests

--- test/cli_spec.js
+++ test/cli_spec.js
@@ -1,12 +1,63 @@
-var fork = require('child_process').fork
-var path = require('path')
+var execFile = require('child_process').execFile
+var child;
+var path = require('path');
+var chai = require('chai');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var sinon  = require("sinon");
+var Pitchfork = require("../index");
 
-var msgs = []
+// GLOBAL CONSTANTS
+var cli_filepath = path.resolve(__dirname+"/../bin/pitchfork");
+var artist_name = "mogwai";
+var album_title = "come on";
+var valid_args = ["-a", artist_name, "-t", album_title];
+var review;
+var search;
 
-describe("Pitchfork CLI (Command Line Tool)", function(){
+chai.use(require('chai-as-promised'))
 
-  it("should return truncated json")
+describe("CLI (Command Line Tool)", function(){
 
-  it("should return usage when no flags are passed")
+  before(function(done){
+    search = new Pitchfork.Search(artist_name, album_title);
+    search.promise.then(function(){
+      search.results[0].promise.then(function(rev){
+        review = rev;
+        done();
+      })
+    })
+  })
+
+  describe("when searching for a valid artist and album", function(){
+
+    it("should return truncated and valid json", function(done){
+      var proc = execFile(cli_filepath, valid_args, function(err, stdout, stderr){
+        expect(stdout).to.equal(JSON.stringify(review.truncated())+"\n")
+        done();
+      })
+    })
+
+  })
+
+  describe("when searching for no artist and no album", function(){
+
+    it("should return usage when no flags are passed", function(done){
+      var proc = execFile(cli_filepath, [], function(err, stdout, stderr){
+        expect(stdout).to.equal(Pitchfork.USAGE+"\n")
+        done();
+      })
+    })
+
+    it("should return the right version when -V flag is passed", function(done){
+      var proc = execFile(cli_filepath, ["-V"], function(err, stdout, stderr){
+        expect(stdout).to.equal(Pitchfork.VERSION+"\n")
+        done();
+      })
+    })
+
+  })
+
+  
 
 })
