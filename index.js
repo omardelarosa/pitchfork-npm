564834fe419bb862a2e744ed4f3e4d4b7499154c
omardelarosa <thedelarosa@gmail.com>
2014-07-16 14:20:14 -0400

    refactored cli stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('superagent');
 var q = require('q');
+var async = require('async');
 
 var origArgs = process.argv;
 
@@ -30,15 +31,22 @@ var artist = parsedArgs.artist;
 var album = parsedArgs.album;
 
 // if there are args (i.e. running in commandline mode)
-if (artist && album) {
-  s = new Pitchfork.Search(artist, album);
-  s.promise.then(function(){
-    // console.log(JSON.stringify(s.results[0]));
-    var rev = s.results[0];
-    rev.promise.then(function(){
-      console.log(JSON.stringify(rev.attributes))
+
+// if in "help mode"
+if (origArgs.indexOf("-h") != -1 || origArgs.length <= 4) {
+  // display usage
+  console.log("usage: pitchfork [-h] -a ARTIST_NAME -t ALBUM_TITLE")
+
+// if there's an artist and/or album
+} else if (artist || album) {
+   s = new Pitchfork.Search(artist, album);
+    s.promise.then(function(){
+      // console.log(JSON.stringify(s.results[0]));
+      var rev = s.results[0];
+      rev.promise.then(function(){
+        console.log(JSON.stringify(rev.attributes))
+      })
     })
-  })
 }
 
 module.exports = Pitchfork;
