de10d85096b2225e84230ebbdc057187b7219808
David Goussev <dgoussev@gmail.com>
2017-01-04 14:01:33 -0800

    implement usage result w no args in req, edit cli_spec

--- test/cli_spec.js
+++ test/cli_spec.js
@@ -8,10 +8,9 @@ var execFile = require('child_process').execFile
 // globals
 var cli_filepath = path.resolve(__dirname+"/../bin/pitchfork");
 var artist_name = "mogwai";
-var album_title = "come on";
+var album_title = "come on die young";
 var valid_args = ["-a", artist_name, "-t", album_title];
 var valid_args_no_flags = [artist_name, album_title];
-var verbose_args = ["-a", artist_name, "-t", album_title, "-v"];
 var review;
 var search;
 
@@ -40,7 +39,7 @@ describe("CLI (Command Line Tool)", function(){
       })
     })
 
-    xit("should return all attributes and valid, ugly json with --json flag", function(done){
+    it("should return all attributes and valid, ugly json with --json flag", function(done){
       var newArgs = valid_args_no_flags
       newArgs.push("--json")
       var proc = execFile(cli_filepath, newArgs , function(err, stdout, stderr){
@@ -49,9 +48,9 @@ describe("CLI (Command Line Tool)", function(){
       })
     })
 
-    xit("should return entire review object and valid json when -v flag is passed", function(done){
+    it("should return entire review object and valid json when -v flag is passed", function(done){
       var newArgs = valid_args_no_flags
-      newArgs.push("-v --json")
+      newArgs.push("-v")
       var proc = execFile(cli_filepath, newArgs, function(err, stdout, stderr){
         expect(stdout).to.equal(JSON.stringify(review.verbose())+"\n")
         done();
@@ -60,9 +59,9 @@ describe("CLI (Command Line Tool)", function(){
 
   })
 
-  xdescribe("when searching for a valid artist and album", function(){
+  describe("when searching for a valid artist and album", function(){
     
-    it("should return all attributes and pretty json by default", function(done){
+    xit("should return all attributes and pretty json by default", function(done){
       var proc = execFile(cli_filepath, valid_args, function(err, stdout, stderr){        
         expect(stdout).to.equal(prettyjson.render(review.attributes)+"\n")
         done();
@@ -79,8 +78,10 @@ describe("CLI (Command Line Tool)", function(){
     })
 
     it("should return entire review object and valid json when -v flag is passed", function(done){
-      var proc = execFile(cli_filepath, verbose_args, function(err, stdout, stderr){
-        expect(stdout).to.equal(prettyjson.render(review.verbose())+"\n")
+      var newArgs = valid_args
+      newArgs.push("-v")
+      var proc = execFile(cli_filepath, newArgs, function(err, stdout, stderr){
+        expect(stdout).to.equal(JSON.stringify(review.verbose())+"\n")
         done();
       })
     })
@@ -89,7 +90,7 @@ describe("CLI (Command Line Tool)", function(){
 
   describe("and when searching for no artist and no album", function(){
 
-    xit("should return usage when no flags are passed", function(done){
+    it("should return usage when no flags are passed", function(done){
       var proc = execFile(cli_filepath, [], function(err, stdout, stderr){
         expect(stdout).to.equal("usage: pitchfork [-hjTvVp] [-tx, --text] -a ARTIST_NAME -t ALBUM_TITLE\n")
         done();
